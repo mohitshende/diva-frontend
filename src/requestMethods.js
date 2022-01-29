30fd923cc82d5d443df32186e50a99b5c4224a5c
Mohit Shende <mohitshende023@gmail.com>
2022-01-29 19:15:58 +0530

    resolved local storage issues

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,8 +2,14 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser?.accessToken;
+let TOKEN;
+
+if (localStorage.getItem("persist:root")) {
+  TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+    .currentUser?.accessToken;
+} else {
+  TOKEN = "";
+}
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
